18a83bcf69a408e50a0d2564261d9bfd32adcbb5
nicolasherrerac <nicolasherrerac20@gmail.com>
2021-04-04 15:51:15 -0500

    Puppet lint

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,10 @@
 # create file in /tmp
 
 file { 'holberton':
-	path => '/tmp/holberton';
-	ensure => 'present';
-	mode => '0744';
-	owner => 'www-data';
-	group => 'www-data';
-	content => 'I love Puppet';
+    ensure  => 'present',
+    path    => '/tmp/holberton',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => 'I love Puppet',
 }
