335fd86eb6a145870dab003dbfada65d3cfd2449
nicolasherrerac <nicolasherrerac20@gmail.com>
2021-04-04 15:34:59 -0500

    Add =>

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,10 @@
 # create file in /tmp
 
 file { 'holberton';
-	path '/tmp/holberton'
-	ensure 'present';
-	mode '0744';
-	owner 'www-data';
-	group 'www-data';
-	content 'I love Puppet';
+	path => '/tmp/holberton'
+	ensure => 'present';
+	mode => '0744';
+	owner => 'www-data';
+	group => 'www-data';
+	content => 'I love Puppet';
 }
