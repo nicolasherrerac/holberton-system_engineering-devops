a5a5526f85b660b14cf6f4fd92a2b0a6e1fa3e3f
nicolasherrerac <nicolasherrerac20@gmail.com>
2021-05-03 15:47:26 -0500

    Export to CSV

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""script to export data in the CSV format."""
+import csv
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    user_id = int(argv[1])
+
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                        .format(user_id)).json()
+
+    todo = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}"
+                        .format(user_id)).json()
+
+
+    with open("{}.csv".format(user_id), 'w') as csvf:
+        fill = csv.writer(csvf, quoting=csv.QUOTE_ALL)
+
+        for task in todo:
+            fill.writerow([user_id, user.get('username'),
+                           task.get('completed'), task.get('title')])
